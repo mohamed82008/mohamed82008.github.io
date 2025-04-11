40bdb5411631997cde29d9c1ad7d662fc290f440
Mohamed Tarek <mohamed82008@gmail.com>
2025-04-11 05:12:44 +0400

    deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,16 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import remarkToc from 'remark-toc';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mohamed82008.github.io/',
   vite: {
     plugins: [tailwindcss()],
   },
   markdown: {
     remarkPlugins: [ [remarkToc, { heading: "content", maxDepth: 2, ordered: true} ] ],
-  }
+  },
+  integrations: [icon()],
 });
