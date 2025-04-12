cd1184fd989015b8cd89e86f7aab59210fd251bc
Mohamed Tarek <mohamed82008@gmail.com>
2025-04-12 16:06:18 +0400

    better scrolling and clickable headings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import remarkToc from 'remark-toc';
+import remarkToc from "remark-toc";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mohamed82008.github.io/',
+  site: "https://mohamed82008.github.io/",
   vite: {
     plugins: [tailwindcss()],
   },
   markdown: {
-    remarkPlugins: [ [remarkToc, { heading: "content", maxDepth: 2, ordered: true} ] ],
+    remarkPlugins: [
+      [remarkToc, { heading: "content", maxDepth: 2, ordered: true }],
+    ],
   },
   integrations: [icon()],
 });
