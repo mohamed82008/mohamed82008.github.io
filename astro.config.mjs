1743a6f1c56c0efda0662e8af5694afe56b14c39
Mohamed Tarek <mohamed82008@gmail.com>
2025-05-05 10:09:16 +0400

    remove remark-toc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import remarkToc from "remark-toc";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://mohamed82008.github.io/",
-  markdown: {
-    remarkPlugins: [
-      [remarkToc, { heading: "content", maxDepth: 2, ordered: true }],
-    ],
-  },
   integrations: [icon()],
 });
