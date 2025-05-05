434b1165c89997b5923bf5a515fe5b1202e792da
Mohamed Tarek <mohamed82008@gmail.com>
2025-05-05 10:06:16 +0400

    remove tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
 import remarkToc from "remark-toc";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://mohamed82008.github.io/",
-  vite: {
-    plugins: [tailwindcss()],
-  },
   markdown: {
     remarkPlugins: [
       [remarkToc, { heading: "content", maxDepth: 2, ordered: true }],
